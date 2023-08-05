ea1e94c9ff5d2f17b08e088e9705c609b7d584e6
harunabhi <harunabhi37@gmail.com>
2023-08-05 22:20:22 +0600

    added lower and uppercase

---
+++ string.js
@@ -0,0 +1,17 @@
+const str = "KEMON aso tmi i love you and i wanna fuck ";
+const length = str.length;
+console.log(length);
+const index = str[39];
+console.log(index);
+const lower = str.toLowerCase();
+console.log(lower);
+const upper = str.toUpperCase();
+console.log(upper);
+const username = 'black pussy';
+const input = 'white pussy';
+if(username==input){
+    console.log("valid user")
+}
+else{
+    console.log("invalid")
+}
